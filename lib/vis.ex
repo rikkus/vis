6a314dddfdbd577cde9d274e2f98dccbe717f28e
Rik Hemsley <rik.hemsley@experian.com>
2020-09-18 15:06:51 +0100

    works

--- lib/vis.ex
+++ lib/vis.ex
@@ -1,12 +1,22 @@
 alias Graphvix.{Graph, Node, Edge, Cluster}
 
 defmodule Vis do
-  def main([input]) do
-    IO.inspect(parse(input))
+  def main([input]),
+   do: to_graphviz(input |> parse)
+   |> IO.puts
+
+  def to_graphviz(list),
+    do: _to_graphviz(Graph.new(), nil, list)
+    |> Graph.to_dot()
+
+  def _to_graphviz(graph, _, nil), do: graph
+  def _to_graphviz(graph, parent_node, [x, l, r]) do
+    {graph, node} = Graph.add_vertex(graph, x)
+    {graph, _} = Graph.add_edge(graph, node, parent_node)
+    graph = _to_graphviz(graph, node, l)
+    graph = _to_graphviz(graph, node, r)
+    graph
   end
-
-  def to_graphviz(list) do
-
 
   @spec parse(binary) :: list
   def parse(str) do
